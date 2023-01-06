c2e4c5f0fccf081e72c8120074a6633a0b06e1eb
Abdenour Touat <61945965+abdennour-touat@users.noreply.github.com>
2023-01-06 21:51:32 +0100

    Develop (#2)
    * adding mui
    
    * adding react router
    
    * testing release 0.1.0
    
    * testing...

--- src-tauri/src/store/mod.rs
+++ src-tauri/src/store/mod.rs
@@ -22,121 +22,129 @@ pub trait Filterable: Into<Value> {}
 
 /// Store struct normalizing CRUD SurrealDB application calls
 pub struct Store {
-	ds: Datastore,
-	ses: Session,
+    ds: Datastore,
+    ses: Session,
 }
 
 impl Store {
-	pub async fn new() -> Result<Self> {
-		let ds = Datastore::new("memory").await?;
-		let ses = Session::for_db("appns", "appdb");
-		Ok(Store { ds, ses })
-	}
+    pub async fn new() -> Result<Self> {
+        let ds = Datastore::new("memory").await?;
+        let ses = Session::for_db("appns", "appdb");
+        Ok(Store { ds, ses })
+    }
 
-	pub async fn exec_get(&self, tid: &str) -> Result<Object> {
-		let sql = "SELECT * FROM $th";
+    pub async fn exec_get(&self, tid: &str) -> Result<Object> {
+        let sql = "SELECT * FROM $th";
 
-		let vars = map!["th".into() => thing(tid)?.into()];
+        let vars = map!["th".into() => thing(tid)?.into()];
 
-		let ress = self.ds.execute(sql, &self.ses, Some(vars), true).await?;
+        let ress = self.ds.execute(sql, &self.ses, Some(vars), true).await?;
 
-		let first_res = ress.into_iter().next().expect("Did not get a response");
+        let first_res = ress.into_iter().next().expect("Did not get a response");
 
-		W(first_res.result?.first()).try_into()
-	}
+        W(first_res.result?.first()).try_into()
+    }
 
-	pub async fn exec_create<T: Creatable>(&self, tb: &str, data: T) -> Result<String> {
-		let sql = "CREATE type::table($tb) CONTENT $data RETURN id";
+    pub async fn exec_create<T: Creatable>(&self, tb: &str, data: T) -> Result<String> {
+        let sql = "CREATE type::table($tb) CONTENT $data RETURN id";
 
-		let mut data: Object = W(data.into()).try_into()?;
-		let now = Datetime::default().timestamp_nanos();
-		data.insert("ctime".into(), now.into());
+        let mut data: Object = W(data.into()).try_into()?;
+        let now = Datetime::default().timestamp_nanos();
+        data.insert("ctime".into(), now.into());
 
-		let vars = map![
+        let vars = map![
 			"tb".into() => tb.into(),
 			"data".into() => Value::from(data)];
 
-		let ress = self.ds.execute(sql, &self.ses, Some(vars), false).await?;
-		let first_val = ress.into_iter().next().map(|r| r.result).expect("id not returned")?;
+        let ress = self.ds.execute(sql, &self.ses, Some(vars), false).await?;
+        let first_val = ress
+            .into_iter()
+            .next()
+            .map(|r| r.result)
+            .expect("id not returned")?;
 
-		if let Value::Object(mut val) = first_val.first() {
-			val.x_take_val::<String>("id")
-				.map_err(|ex| Error::StoreFailToCreate(f!("exec_create {tb} {ex}")))
-		} else {
-			Err(Error::StoreFailToCreate(f!("exec_create {tb}, nothing returned.")))
-		}
-	}
+        if let Value::Object(mut val) = first_val.first() {
+            val.x_take_val::<String>("id")
+                .map_err(|ex| Error::StoreFailToCreate(f!("exec_create {tb} {ex}")))
+        } else {
+            Err(Error::StoreFailToCreate(f!(
+                "exec_create {tb}, nothing returned."
+            )))
+        }
+    }
 
-	pub async fn exec_merge<T: Patchable>(&self, tid: &str, data: T) -> Result<String> {
-		let sql = "UPDATE $th MERGE $data RETURN id";
+    pub async fn exec_merge<T: Patchable>(&self, tid: &str, data: T) -> Result<String> {
+        let sql = "UPDATE $th MERGE $data RETURN id";
 
-		let vars = map![
+        let vars = map![
 			"th".into() => thing(tid)?.into(),
 			"data".into() => data.into()];
 
-		let ress = self.ds.execute(sql, &self.ses, Some(vars), true).await?;
+        let ress = self.ds.execute(sql, &self.ses, Some(vars), true).await?;
 
-		let first_res = ress.into_iter().next().expect("id not returned");
+        let first_res = ress.into_iter().next().expect("id not returned");
 
-		let result = first_res.result?;
+        let result = first_res.result?;
 
-		if let Value::Object(mut val) = result.first() {
-			val.x_take_val("id")
-		} else {
-			Err(Error::StoreFailToCreate(f!("exec_merge {tid}, nothing returned.")))
-		}
-	}
+        if let Value::Object(mut val) = result.first() {
+            val.x_take_val("id")
+        } else {
+            Err(Error::StoreFailToCreate(f!(
+                "exec_merge {tid}, nothing returned."
+            )))
+        }
+    }
 
-	pub async fn exec_delete(&self, tid: &str) -> Result<String> {
-		let sql = "DELETE $th";
+    pub async fn exec_delete(&self, tid: &str) -> Result<String> {
+        let sql = "DELETE $th";
 
-		let vars = map!["th".into() => thing(tid)?.into()];
+        let vars = map!["th".into() => thing(tid)?.into()];
 
-		let ress = self.ds.execute(sql, &self.ses, Some(vars), false).await?;
+        let ress = self.ds.execute(sql, &self.ses, Some(vars), false).await?;
 
-		let first_res = ress.into_iter().next().expect("Did not get a response");
+        let first_res = ress.into_iter().next().expect("Did not get a response");
 
-		// Return the error if result failed
-		first_res.result?;
+        // Return the error if result failed
+        first_res.result?;
 
-		// return success
-		Ok(tid.to_string())
-	}
+        // return success
+        Ok(tid.to_string())
+    }
 
-	pub async fn exec_select(&self, tb: &str, filter: Option<Value>) -> Result<Vec<Object>> {
-		let mut sql = String::from("SELECT * FROM type::table($tb)");
+    pub async fn exec_select(&self, tb: &str, filter: Option<Value>) -> Result<Vec<Object>> {
+        let mut sql = String::from("SELECT * FROM type::table($tb)");
 
-		let mut vars = BTreeMap::from([("tb".into(), tb.into())]);
+        let mut vars = BTreeMap::from([("tb".into(), tb.into())]);
 
-		// --- Apply the filter
-		if let Some(filter) = filter {
-			let obj: Object = W(filter).try_into()?;
-			sql.push_str(" WHERE");
-			for (idx, (k, v)) in obj.into_iter().enumerate() {
-				let var = f!("w{idx}");
-				sql.push_str(&f!(" {k} = ${var}"));
-				vars.insert(var, v);
-			}
-		}
+        // --- Apply the filter
+        if let Some(filter) = filter {
+            let obj: Object = W(filter).try_into()?;
+            sql.push_str(" WHERE");
+            for (idx, (k, v)) in obj.into_iter().enumerate() {
+                let var = f!("w{idx}");
+                sql.push_str(&f!(" {k} = ${var}"));
+                vars.insert(var, v);
+            }
+        }
 
-		// --- Apply the orderby
-		sql.push_str(" ORDER ctime DESC");
+        // --- Apply the orderby
+        sql.push_str(" ORDER ctime DESC");
 
-		let ress = self.ds.execute(&sql, &self.ses, Some(vars), false).await?;
+        let ress = self.ds.execute(&sql, &self.ses, Some(vars), false).await?;
 
-		let first_res = ress.into_iter().next().expect("Did not get a response");
+        let first_res = ress.into_iter().next().expect("Did not get a response");
 
-		// Get the result value as value array (fail if it is not)
-		let array: Array = W(first_res.result?).try_into()?;
+        // Get the result value as value array (fail if it is not)
+        let array: Array = W(first_res.result?).try_into()?;
 
-		// build the list of objects
-		array.into_iter().map(|value| W(value).try_into()).collect()
+        // build the list of objects
+        array.into_iter().map(|value| W(value).try_into()).collect()
 
-		// Note: Above equivalent to
-		// let mut objs: Vec<Object> = Vec::new();
-		// for item in array.into_iter() {
-		// 	objs.push(W(item).try_into()?);
-		// }
-		// Ok(objs)
-	}
+        // Note: Above equivalent to
+        // let mut objs: Vec<Object> = Vec::new();
+        // for item in array.into_iter() {
+        // 	objs.push(W(item).try_into()?);
+        // }
+        // Ok(objs)
+    }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,25 @@ import { useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 import { ipc_invoke } from "./ipc";
 import { projectFmc } from "./model";
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
+import { Project } from "./bindings";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  const [projects, setProjects] = useState<Project[]>([]);
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
   }
+  async function listProject() {
+    let res = await projectFmc.list();
+    setProjects(res);
+  }
   async function createProject() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    // projectFmc.create({ name: "test" });
-    let res = await projectFmc.list();
-    console.log(res);
+    await projectFmc.create({ name: "test" });
   }
 
   return (
@@ -24,6 +28,14 @@ function App() {
       <Typography variant="h3">
         hello world this is a react+mui+react-router+ tauri+rust+surrealdb
       </Typography>
+      <Button onClick={() => createProject()}>create</Button>
+      <Button onClick={() => listProject()}>list</Button>
+      {projects.map((prj) => (
+        <div>
+          <h3>{prj.id}</h3>
+          <h3>{prj.name}</h3>
+        </div>
+      ))}
     </div>
   );
 }
