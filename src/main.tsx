ee37f0d9e96be3151ab3b3077c777b55d024dcfc
Abdenour Touat <61945965+abdennour-touat@users.noreply.github.com>
2023-01-06 12:06:37 +0100

    adding react router

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./style.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./event";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
